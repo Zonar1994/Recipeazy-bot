4d2270e54e10b7d1b2fc2b34b8edd68797892b9f
Zonar94 <zonar199421@gmail.com>
2023-03-16 12:26:15 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import openai
+
+client = discord.Client(intents=discord.Intents().all())
+
+# Set up the OpenAI API
+openai.api_key = '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'
+
+# Define the virtual chef's name and the model ID for ChatGPT
+chef_name = 'Virtual Chef'
+model_id = 'Ramsey'
+
+# Handle incoming messages from users
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Generate a response using ChatGPT
+    prompt = f'{chef_name}: {message.content}'
+    response = openai.Completion.create(
+        engine=model_id,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    # Send the response back to the user
+    await message.channel.send(f'{message.author}: {response.choices[0].text}')
+
+# Run the bot
+client.run('MTA4NTg2NDQ0NDgxNjUyNzQxMA.G0wAqt.fxXsX-q2Yr75tlmvtOq-q65vtuLluNQZ1PRDBU')
